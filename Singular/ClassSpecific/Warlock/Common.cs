7325e866a9359bf6ad046e9b2d4d97dc3c4aa255
ApocDev <ApocDev@users.noreply.github.com>
2011-02-07 13:08:22 +0000

    Common warlock buff added

---
+++ Singular/ClassSpecific/Warlock/Common.cs
@@ -0,0 +1,20 @@
+﻿using Styx.Combat.CombatRoutine;
+using Styx.Logic.Combat;
+
+using TreeSharp;
+
+namespace Singular
+{
+    partial class SingularRoutine
+    {
+        [Class(WoWClass.Warlock), Spec(TalentSpec.AfflictionWarlock), Spec(TalentSpec.DemonologyWarlock), Spec(TalentSpec.DestructionWarlock), Behavior(BehaviorType.PreCombatBuffs), Context(WoWContext.All)]
+        public Composite CreateWarlockBuffs()
+        {
+            return new PrioritySelector(
+                CreateSpellBuffOnSelf("Demon Armor", ret=> !SpellManager.HasSpell("Fel Armor")),
+                CreateSpellBuffOnSelf("Fel Armor"),
+                CreateSpellBuffOnSelf("Soul Link")
+                );
+        }
+    }
+}

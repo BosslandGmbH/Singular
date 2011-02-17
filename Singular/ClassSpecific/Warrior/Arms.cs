d26e555f72ec1bf8f7bc07789796aca716cf9239
ApocDev <ApocDev@users.noreply.github.com>
2011-02-17 09:04:11 +0000

    Quick warrior prototypes

---
+++ Singular/ClassSpecific/Warrior/Arms.cs
@@ -0,0 +1,53 @@
+﻿#region Revision Info
+
+// This file is part of Singular - A community driven Honorbuddy CC
+// $Author$
+// $Date$
+// $HeadURL$
+// $LastChangedBy$
+// $LastChangedDate$
+// $LastChangedRevision$
+// $Revision$
+
+#endregion
+
+using System.Linq;
+
+using Styx.Combat.CombatRoutine;
+
+using TreeSharp;
+
+namespace Singular
+{
+    partial class SingularRoutine
+    {
+        [Class(WoWClass.Warrior)]
+        [Spec(TalentSpec.ArmsWarrior)]
+        [Context(WoWContext.All)]
+        [Behavior(BehaviorType.Combat)]
+        public Composite CreateArmsWarriorCombat()
+        {
+            return new PrioritySelector(
+                CreateEnsureTarget(),
+                CreateRangeAndFace(4f, ret => Me.CurrentTarget),
+                new Decorator(
+                    ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 6) > 2,
+                    new PrioritySelector(
+                        CreateSpellBuff("Rend"),
+                        CreateSpellCast("Thunderclap"),
+                        CreateSpellCast("Sweeping Strikes"),
+                        CreateSpellCast("Bladestorm"),
+                        CreateSpellCast("Cleave"),
+                        CreateSpellCast("Whirlwind")
+                        )),
+                CreateSpellCast("Rend", ret => HasAuraStacks("Overpower", 1)),
+                CreateSpellCast("Collossus Smash"),
+                CreateSpellCast("Execute", ret => Me.CurrentTarget.HealthPercent < 20),
+                CreateSpellCast("Overpower"),
+                CreateSpellCast("Mortal Strike"),
+                CreateSpellCast("Slam"),
+                CreateSpellCast("Heroic Strike", ret => Me.RagePercent > 60)
+                );
+        }
+    }
+}
---
+++ Singular/ClassSpecific/Warrior/Fury.cs
@@ -0,0 +1,49 @@
+﻿#region Revision Info
+
+// This file is part of Singular - A community driven Honorbuddy CC
+// $Author$
+// $Date$
+// $HeadURL$
+// $LastChangedBy$
+// $LastChangedDate$
+// $LastChangedRevision$
+// $Revision$
+
+#endregion
+
+using System.Linq;
+
+using Styx.Combat.CombatRoutine;
+
+using TreeSharp;
+
+namespace Singular
+{
+    partial class SingularRoutine
+    {
+        [Class(WoWClass.Warrior)]
+        [Spec(TalentSpec.FuryWarrior)]
+        [Context(WoWContext.All)]
+        [Behavior(BehaviorType.Combat)]
+        public Composite CreateWarriorFuryCombat()
+        {
+            return new PrioritySelector(
+                CreateEnsureTarget(),
+                CreateRangeAndFace(4f, ret => Me.CurrentTarget),
+                new Decorator(
+                    ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 6) > 2,
+                    new PrioritySelector(
+                        CreateSpellCast("Cleave"),
+                        CreateSpellCast("Whirlwind"),
+                        CreateSpellCast("Bloodthirst")
+                        )),
+                CreateSpellCast("Collossus Smash"),
+                CreateSpellCast("Execute", ret => Me.CurrentTarget.HealthPercent < 20),
+                CreateSpellCast("Bloodthirst"),
+                CreateSpellCast("Raging Blow"),
+                CreateSpellCast("Slam", ret => HasAuraStacks("Blood Surge", 1)),
+                CreateSpellCast("Heroic Strike", ret => Me.RagePercent > 60)
+                );
+        }
+    }
+}

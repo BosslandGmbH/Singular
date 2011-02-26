25e5311cf4df246d5e9b48b42d8be3615ffcecf5
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2011-02-26 12:45:42 +0000

    - Should now stop summoning Felguard if it appears while summoning

--- Singular/ClassSpecific/Warlock/Common.cs
+++ Singular/ClassSpecific/Warlock/Common.cs
@@ -30,19 +30,23 @@ namespace Singular
         [Context(WoWContext.All)]
         public Composite CreateWarlockBuffs()
         {
-            return new PrioritySelector(
-                CreateWaitForCast(),
-                CreateSpellBuffOnSelf("Demon Armor", ret => !Me.HasAura("Demon Armor") && !SpellManager.HasSpell("Fel Armor")),
-                CreateSpellBuffOnSelf("Fel Armor", ret => !Me.HasAura("Fel Armor")),
-                // Soul Link is considered "passive". It has no duration (thus, no end-time)
-                // We have to check it this way, or we'll keep casting it over and over and over
-                CreateSpellBuffOnSelf("Soul Link", ret => !Me.HasAura("Soul Link") && Me.GotAlivePet),
-                CreateSpellCast("Soul Harvest", ret => Me.CurrentSoulShards < 2),
-                //new ActionLogMessage(false, "Checking for pet"),
-                new Decorator(
-                    ret => !Me.GotAlivePet,
-                    new Action(ret => PetManager.CallPet(WantedPet)))
-                );
+			return new PrioritySelector(
+				new Decorator(
+					ctx => Me.CastingSpell != null && Me.CastingSpell.Name == "Summon " + WantedPet && Me.GotAlivePet,
+					new Action(ctx => SpellManager.StopCasting())),
+
+				CreateWaitForCast(),
+				CreateSpellBuffOnSelf("Demon Armor", ret => !Me.HasAura("Demon Armor") && !SpellManager.HasSpell("Fel Armor")),
+				CreateSpellBuffOnSelf("Fel Armor", ret => !Me.HasAura("Fel Armor")),
+				// Soul Link is considered "passive". It has no duration (thus, no end-time)
+				// We have to check it this way, or we'll keep casting it over and over and over
+				CreateSpellBuffOnSelf("Soul Link", ret => !Me.HasAura("Soul Link") && Me.GotAlivePet),
+				CreateSpellCast("Soul Harvest", ret => Me.CurrentSoulShards < 2),
+				//new ActionLogMessage(false, "Checking for pet"),
+				new Decorator(
+					ret => !Me.GotAlivePet,
+					new Action(ret => PetManager.CallPet(WantedPet)))
+					);
         }
     }
 }

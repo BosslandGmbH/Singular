f77accd5367de95ab3fc1fde1586581e024be8b0
ApocDev <ApocDev@users.noreply.github.com>
2011-02-28 12:27:31 +0000

    Forced resto to allow Lifebloom on multiple targets when ToL is active.

--- Singular/ClassSpecific/Druid/Resto.cs
+++ Singular/ClassSpecific/Druid/Resto.cs
@@ -16,6 +16,7 @@ using System.Linq;
 
 using Singular.Settings;
 
+using Styx;
 using Styx.Combat.CombatRoutine;
 using Styx.Logic;
 using Styx.Logic.Combat;
@@ -51,70 +52,89 @@ namespace Singular
 		{
 		    NeedHealTargeting = true;
 		    const uint MAPLE_SEED_ID = 17034;
-			return new
-				Decorator(
-				ret => HealTargeting.Instance.FirstUnit != null,
-				new PrioritySelector(
-					ctx => HealTargeting.Instance.FirstUnit,
-					CreateWaitForCast(),
-				// Ensure we're in range of the unit to heal, and it's in LOS.
-					CreateRangeAndFace(35f, ret => (WoWUnit)ret),
-					//Cast Lifebloom on tank if
-					//1- Tank doesn't have lifebloom
-					//2- Tank has less then 3 stacks of lifebloom
-					//3- Tank has 3 stacks of lifebloom but it will expire in 3 seconds
-					CreateSpellCast("Lifebloom", 
-						ret => Me.Combat && RaFHelper.Leader != null && (WoWUnit)ret == RaFHelper.Leader &&
-							   ((WoWUnit)ret).HealthPercent > 60 && 
-							   (!((WoWUnit)ret).HasAura("Lifebloom") || ((WoWUnit)ret).Auras["Lifebloom"].StackCount < 3 ||
-							   ((WoWUnit)ret).Auras["Lifebloom"].TimeLeft <= TimeSpan.FromSeconds(3)),
-						ret => (WoWUnit)ret),
+		    return new
+		        Decorator(
+		        ret => HealTargeting.Instance.FirstUnit != null,
+		        new PrioritySelector(
+		            ctx => HealTargeting.Instance.FirstUnit,
+		            CreateWaitForCast(),
+		            // Ensure we're in range of the unit to heal, and it's in LOS.
+		            CreateRangeAndFace(35f, ret => (WoWUnit)ret),
+		            //Cast Lifebloom on tank if
+		            //1- Tank doesn't have lifebloom
+		            //2- Tank has less then 3 stacks of lifebloom
+		            //3- Tank has 3 stacks of lifebloom but it will expire in 3 seconds
+		            CreateSpellCast(
+		                "Lifebloom",
+		                ret =>
+		                Me.Combat && 
+                        // Keep 3 stacks up on the tank/leader at all times.
+                        // If we're in ToL form, we can do rolling LBs for everyone. So ignore the fact that its the leader or not.
+                        // LB is cheap, and VERY powerful in ToL form since you can spam it on the entire raid, for a cheap HoT and quite good 'bloom'
+                        ((RaFHelper.Leader != null && (WoWUnit)ret == RaFHelper.Leader) || Me.Shapeshift == ShapeshiftForm.TreeOfLife) &&
+		                ((WoWUnit)ret).HealthPercent > 60 &&
+		                (!((WoWUnit)ret).HasAura("Lifebloom") || ((WoWUnit)ret).Auras["Lifebloom"].StackCount < 3 ||
+		                 ((WoWUnit)ret).Auras["Lifebloom"].TimeLeft <= TimeSpan.FromSeconds(3)),
+		                ret => (WoWUnit)ret),
 
-					//Cast rebirth if the tank is dead. Check for Unburdened Rebirth glyph or Maple seed reagent
-					CreateSpellCast("Rebirth",
-						ret => Me.Combat && RaFHelper.Leader != null && (WoWUnit)ret == RaFHelper.Leader &&
-							   ((WoWUnit)ret).Dead && (TalentManager.HasGlyph("Unburdened Rebirth") || Me.BagItems.Any(i => i.Entry == MAPLE_SEED_ID)),
-						ret => (WoWUnit)ret),
+		            //Cast rebirth if the tank is dead. Check for Unburdened Rebirth glyph or Maple seed reagent
+		            CreateSpellCast(
+		                "Rebirth",
+		                ret => Me.Combat && RaFHelper.Leader != null && (WoWUnit)ret == RaFHelper.Leader &&
+		                       ((WoWUnit)ret).Dead && (TalentManager.HasGlyph("Unburdened Rebirth") || Me.BagItems.Any(i => i.Entry == MAPLE_SEED_ID)),
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Tranquility",
-						ret => Me.Combat && Me.IsInParty && NearbyFriendlyPlayers.Count(p =>
-								p.IsAlive && p.HealthPercent <= SingularSettings.Instance.Druid.TranquilityHealth && p.Distance <= 30) >= SingularSettings.Instance.Druid.TranquilityCount),
+		            CreateSpellCast(
+		                "Tranquility",
+		                ret => Me.Combat && Me.IsInParty && NearbyFriendlyPlayers.Count(
+		                    p =>
+		                    p.IsAlive && p.HealthPercent <= SingularSettings.Instance.Druid.TranquilityHealth && p.Distance <= 30) >=
+		                       SingularSettings.Instance.Druid.TranquilityCount),
 
-					//Use Innervate on party members if we have Glyph of Innervate
-					CreateSpellBuff("Innervate",
-						ret => TalentManager.HasGlyph("Innervate") && Me.Combat && (WoWUnit)ret != Me && Me.ManaPercent <= SingularSettings.Instance.Druid.InnervateMana && 
-							   ((WoWUnit)ret).PowerType == Styx.WoWPowerType.Mana && ((WoWUnit)ret).ManaPercent <= SingularSettings.Instance.Druid.InnervateMana,
-						ret => (WoWUnit)ret),
+		            //Use Innervate on party members if we have Glyph of Innervate
+		            CreateSpellBuff(
+		                "Innervate",
+		                ret =>
+		                TalentManager.HasGlyph("Innervate") && Me.Combat && (WoWUnit)ret != Me &&
+		                Me.ManaPercent <= SingularSettings.Instance.Druid.InnervateMana &&
+		                ((WoWUnit)ret).PowerType == Styx.WoWPowerType.Mana && ((WoWUnit)ret).ManaPercent <= SingularSettings.Instance.Druid.InnervateMana,
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Swiftmend",
-						ret => Me.Combat && ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Swiftmend && 
-							   (((WoWUnit)ret).HasAura("Rejuvenation") || ((WoWUnit)ret).HasAura("Regrowth")),
-						ret => (WoWUnit)ret),
+		            CreateSpellCast(
+		                "Swiftmend",
+		                ret => Me.Combat && ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Swiftmend &&
+		                       (((WoWUnit)ret).HasAura("Rejuvenation") || ((WoWUnit)ret).HasAura("Regrowth")),
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Wild Growth",
-						ret => Me.IsInParty && NearbyFriendlyPlayers.Count(
-									p => p.IsAlive && p.HealthPercent <= SingularSettings.Instance.Druid.WildGrowthHealth &&
-										 p.Location.Distance(((WoWUnit)ret).Location) <= 30) >= SingularSettings.Instance.Druid.WildGrowthCount,
-						ret => (WoWUnit)ret),
+		            CreateSpellCast(
+		                "Wild Growth",
+		                ret => Me.IsInParty && NearbyFriendlyPlayers.Count(
+		                    p => p.IsAlive && p.HealthPercent <= SingularSettings.Instance.Druid.WildGrowthHealth &&
+		                         p.Location.Distance(((WoWUnit)ret).Location) <= 30) >= SingularSettings.Instance.Druid.WildGrowthCount,
+		                ret => (WoWUnit)ret),
 
-					CreateSpellBuff("Regrowth",
-						ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Regrowth,
-						ret => (WoWUnit)ret),
+		            CreateSpellBuff(
+		                "Regrowth",
+		                ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Regrowth,
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Healing Touch",
-						ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.HealingTouch,
-						ret => (WoWUnit)ret),
+		            CreateSpellCast(
+		                "Healing Touch",
+		                ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.HealingTouch,
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Nourish",
-						ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Nourish && 
-							   (((WoWUnit)ret).HasAura("Rejuvenation") || ((WoWUnit)ret).HasAura("Regrowth") ||
-							   ((WoWUnit)ret).HasAura("Lifebloom") || ((WoWUnit)ret).HasAura("Wild Growth")),
-						ret => (WoWUnit)ret),
+		            CreateSpellCast(
+		                "Nourish",
+		                ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Nourish &&
+		                       (((WoWUnit)ret).HasAura("Rejuvenation") || ((WoWUnit)ret).HasAura("Regrowth") ||
+		                        ((WoWUnit)ret).HasAura("Lifebloom") || ((WoWUnit)ret).HasAura("Wild Growth")),
+		                ret => (WoWUnit)ret),
 
-					CreateSpellCast("Rejuvenation",
-						ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Rejuvenation,
-						ret => (WoWUnit)ret)
-					));
+		            CreateSpellCast(
+		                "Rejuvenation",
+		                ret => ((WoWUnit)ret).HealthPercent <= SingularSettings.Instance.Druid.Rejuvenation,
+		                ret => (WoWUnit)ret)
+		            ));
 		}
 
 		[Class(WoWClass.Druid)]

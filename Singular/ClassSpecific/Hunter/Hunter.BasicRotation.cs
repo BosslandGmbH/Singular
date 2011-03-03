572a85f41b678f37a377401e42134faf70a52dcf
raphudo <raphudo@users.noreply.github.com>
2011-03-03 14:49:25 +0000

    - Hunter lowbie rotation

--- Singular/ClassSpecific/Hunter/Hunter.BasicRotation.cs
+++ Singular/ClassSpecific/Hunter/Hunter.BasicRotation.cs
@@ -21,28 +21,6 @@ namespace Singular
 {
     partial class SingularRoutine
     {
-		[Class(WoWClass.Hunter)]
-		[Spec(TalentSpec.Lowbie)]
-		[Behavior(BehaviorType.Combat)]
-		[Behavior(BehaviorType.Pull)]
-		[Context(WoWContext.All)]
-		public Composite CreateLowbieCombat()
-		{
-			return new PrioritySelector(
-				CreateEnsureTarget(),
-				CreateWaitForCast(),
-				CreateHunterBackPedal(),
-				CreateRangeAndFace(35f, ret => Me.CurrentTarget),
-                CreateAutoAttack(true),
-                // Always keep it up on our target!
-                CreateSpellBuff("Hunter's Mark"),
-                // Heal pet when below 70
-                CreateSpellCast("Mend Pet", ret => Me.Pet.HealthPercent < 70 && !Me.Pet.HasAura("Mend Pet")),
-				CreateSpellCast("Arcane Shot"),
-				CreateSpellCast("Steady Shot")
-				);
-		}
-
         [Class(WoWClass.Hunter)]
 		[Spec(TalentSpec.BeastMasteryHunter)]
         [Behavior(BehaviorType.Combat)]
---
+++ Singular/ClassSpecific/Hunter/Lowbie.cs
@@ -0,0 +1,34 @@
+﻿using Styx.Combat.CombatRoutine;
+
+using TreeSharp;
+using Styx.Logic.Pathing;
+using Styx.Helpers;
+
+namespace Singular
+{
+	partial class SingularRoutine
+	{
+		[Class(WoWClass.Hunter)]
+		[Spec(TalentSpec.Lowbie)]
+		[Behavior(BehaviorType.Combat)]
+		[Behavior(BehaviorType.Pull)]
+		[Context(WoWContext.All)]
+		public Composite CreateLowbieCombat()
+		{
+			return new PrioritySelector(
+				CreateEnsureTarget(),
+				CreateWaitForCast(),
+				CreateHunterBackPedal(),
+				CreateRangeAndFace(35f, ret => Me.CurrentTarget),
+				CreateAutoAttack(true),
+				// Always keep it up on our target!
+				CreateSpellBuff("Hunter's Mark"),
+				// Heal pet when below 70
+				CreateSpellCast("Mend Pet", ret => Me.Pet.HealthPercent < 70 && !Me.Pet.HasAura("Mend Pet")),
+				CreateSpellCast("Arcane Shot"),
+				CreateSpellCast("Steady Shot"),
+				CreateSpellCast("Raptor Strike", ret => Me.CurrentTarget.DistanceSqr < 5 * 5)
+				);
+		}
+	}
+}

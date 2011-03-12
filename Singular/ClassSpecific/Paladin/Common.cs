6743581940ee290f3c535ed0359e762499333aa4
ApocDev <ApocDev@users.noreply.github.com>
2011-03-12 22:31:16 +0000

    Added mopysworld version of ret pally. This is UNTESTED by a core contributor, so use at your own risk. Modified by Apoc to remove silly "AFKMode" stuff. We'll be disabling global movement in a future release via a toggle.

---
+++ Singular/ClassSpecific/Paladin/Common.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+
+using Styx;
+using Styx.Combat.CombatRoutine;
+using Styx.Logic.Combat;
+
+using TreeSharp;
+
+namespace Singular
+{
+    partial class SingularRoutine
+    {
+        private bool NeedKings
+        {
+            get
+            {
+                return Me.PartyMembers.Any(p => p.Class == WoWClass.Druid || (p.Class == WoWClass.Paladin && p.Guid != Me.Guid)) ||
+                       Me.RaidMembers.Any(p => p.Class == WoWClass.Druid || (p.Class == WoWClass.Paladin && p.Guid != Me.Guid));
+            }
+        }
+
+        private bool CurrentTargetIsUndeadOrDemon { get { return Me.CurrentTarget.CreatureType == WoWCreatureType.Undead || Me.CurrentTarget.CreatureType == WoWCreatureType.Demon; } }
+
+        [Class(WoWClass.Paladin)]
+        [Behavior(BehaviorType.PreCombatBuffs)]
+        [Spec(TalentSpec.RetributionPaladin)]
+        [Spec(TalentSpec.HolyPaladin)]
+        [Spec(TalentSpec.ProtectionPaladin)]
+        [Context(WoWContext.All)]
+        public Composite CreatePaladinBuffComposite()
+        {
+            return
+                new PrioritySelector(
+                    // This won't run, but it's here for changes in the future. We NEVER run this method if we're mounted.
+                    CreateSpellBuffOnSelf("Crusader Aura", ret => Me.Mounted),
+
+
+                    // Ghetto Kings Check
+                    CreateSpellBuffOnSelf("Blessing of Might", ret => Me.HasAura("Blessing of Forgotten Kings") && !Me.HasAura("Blessing of Might")),
+                    //Party Buffs
+                    new Decorator(
+                        ret => Me.IsInParty || Me.IsInRaid,
+                        new PrioritySelector(
+                            // Might if Druid or another Pally is in Group or Raid
+                            CreateSpellBuffOnSelf(
+                                "Blessing of Might", ret => NeedKings &&
+                                                            (!Me.HasAura("Blessing of Kings") || Me.Auras["Blessing of Kings"].CreatorGuid != Me.Guid)
+                                                            && !Me.HasAura("Blessing of Might")),
+                            // Kings if no Druid or another Pally is in Group or Raid
+                            CreateSpellBuffOnSelf(
+                                "Blessing of Kings", ret => !NeedKings &&
+                                                            (!Me.HasAura("Blessing of Might") || Me.Auras["Blessing of Might"].CreatorGuid != Me.Guid) &&
+                                                            !Me.HasAura("Embrace of the Shale Spider") &&
+                                                            !Me.HasAura("Mark of the Wild") &&
+                                                            !Me.HasAura("Blessing of Forgotten Kings") &&
+                                                            !Me.HasAura("Blessing of Kings")),
+                            CreateSpellBuffOnSelf("Seal of Truth"),
+                            CreateSpellBuffOnSelf("Retribution Aura", ret => !Me.Mounted)
+                            )),
+                    // Solo Buffs
+                    new Decorator(
+                        ret => !Me.IsInParty || !Me.IsInRaid,
+                        new PrioritySelector(
+                            CreateSpellBuffOnSelf(
+                                "Blessing of Might", ret =>
+                                                     (!Me.HasAura("Blessing of Kings") || Me.Auras["Blessing of Kings"].CreatorGuid != Me.Guid)
+                                                     && !Me.HasAura("Blessing of Might")),
+                            CreateSpellBuffOnSelf(
+                                "Blessing of Kings", ret =>
+                                                     (!Me.HasAura("Blessing of Might") || Me.Auras["Blessing of Might"].CreatorGuid != Me.Guid) &&
+                                                     !Me.HasAura("Embrace of the Shale Spider") &&
+                                                     !Me.HasAura("Mark of the Wild") &&
+                                                     !Me.HasAura("Blessing of Forgotten Kings") &&
+                                                     !Me.HasAura("Blessing of Kings")),
+                            //uncomment Truth line to use it over Insight.
+                            //CreateSpellBuffOnSelf("Seal of Truth"),
+                            CreateSpellBuffOnSelf("Seal of Insight", ret => !Me.HasAura("Seal of Truth")),
+                            CreateSpellBuffOnSelf("Devotion Aura", ret => !Me.Mounted)
+                            )),
+                    CreateSpellBuffOnSelf(
+                        "Seal of Righteousness",
+                        ret =>
+                        (!SpellManager.HasSpell("Seal of Truth") || !SpellManager.HasSpell("Seal of Insight")) && !Me.HasAura("Seal of Righteousness"))
+                    );
+        }
+    }
+}
--- Singular/ClassSpecific/Paladin/Retribution.cs
+++ Singular/ClassSpecific/Paladin/Retribution.cs
@@ -13,9 +13,10 @@
 
 using System.Linq;
 
-using Styx.Combat.CombatRoutine;
-
 using Singular.Settings;
+
+using Styx;
+using Styx.Combat.CombatRoutine;
 
 using TreeSharp;
 
@@ -23,6 +24,27 @@ namespace Singular
 {
     partial class SingularRoutine
     {
+        /* Priority of Spells - Retribution
+
+                 Single 
+         The priority for single-target is as follows: Inq > HoW > Exo > TV > CS > J > HW > Cons
+         Against Undead or Demons: Inq > Exo > HoW > TV > CS > J > HW > Cons
+         
+                 AoE
+         The AoE rotation is rather similar to single-target, simply replace Crusader Strike with Divine Storm.
+         The rest of the priority stays the same. With Holy Wrath fixed to meteor properly, at 3 targets you
+         should begin using Consecration over HW.
+
+         Redcape's modeling shows you should only swap to AoE rotation at 5 or more targets.
+        
+                 Zealotry
+         Zealotry provides a special circumstance. During Zealotry all CS earn 3 HP. 
+         Your rotation will become CS, TV, or should HoL proc, CS, TV, TV.
+        
+         With current values for HoW and Exo, it is currently better to cast a HoW or AoW Exo over a CS or TV even during Zealotry.
+         */
+
+        #region Combat
         [Class(WoWClass.Paladin)]
         [Spec(TalentSpec.RetributionPaladin)]
         [Behavior(BehaviorType.Combat)]
@@ -30,29 +52,94 @@ namespace Singular
         [Context(WoWContext.All)]
         public Composite CreateRetributionPaladinCombat()
         {
-            // Divine Purpose - Tab 3, Index 17
             return
                 new PrioritySelector(
                     CreateEnsureTarget(),
-                    // Make sure we're in range, and facing the damned target. (LOS check as well)
-                    CreateMoveToAndFace(5f, ret => Me.CurrentTarget),
                     CreateAutoAttack(true),
-                    CreateSpellCast("Word of Glory", ret => Me.CurrentHolyPower >= 2 && Me.HealthPercent <= 50),
-                    CreateSpellBuffOnSelf("Inquisition", ret => Me.CurrentHolyPower == 3),
-                    CreateSpellCast("Hammer of Wrath"),
-                    CreateSpellCast("Exorcism", ret => Me.ActiveAuras.ContainsKey("The Art of War")),
-                    CreateSpellCast("Templar's Verdict", ret => Me.CurrentHolyPower == 3),
-                    CreateSpellCast(
-                        "Crusader Strike", ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 8) < 4 || TalentManager.GetCount(3, 17) < 2),
-                    CreateSpellCast(
-                        "Divine Storm", ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 8) >= 4 && TalentManager.GetCount(3, 17) >= 2),
-                    CreateSpellCast("Templar's Verdict", ret => Me.HasAura("Hand of Light")),
-                    CreateSpellCast("Judgement"),
-                    CreateSpellCast("Holy Wrath"),
-                    CreateSpellCast("Consecration", ret => NearbyFriendlyPlayers.Count(u => u.Distance < 8) >= SingularSettings.Instance.Paladin.ConsecrationCount)
+                    CreateRetributionPaladinCombatBuffs(),
+
+                    //Interrupts
+                    new Decorator(
+                        ret => Me.CurrentTarget.IsCasting,
+                        new PrioritySelector(
+                            //Rebuke ID = 96231
+                            CreateSpellCast(96231),
+                            // Calling Rebuke by name FAILS!!!!!!!!!!! sigh :(
+                            //CreateSpellCast("Rebuke", ret => Me.CurrentTarget.IsCasting || Me.CurrentTarget.ChanneledCastingSpellId != null),
+                            CreateSpellCast("Hammer of Justice"),
+                            CreateSpellCast("Arcane Torrent")
+                            )),
+
+                    // Zealotry Routine
+                    new Decorator(
+                        ret => Me.HasAura("Zealotry"),
+                        new PrioritySelector(
+                            CreateSpellBuffOnSelf("Inquisition", ret => Me.CurrentHolyPower > 1),
+                            CreateSpellCast("Hammer of Wrath"),
+                            CreateSpellCast("Exorcism", ret => Me.ActiveAuras.ContainsKey("The Art of War")),
+                            CreateSpellCast("Templar's Verdict", ret => Me.CurrentHolyPower > 2 || Me.ActiveAuras.ContainsKey("Hand of Light")),
+                            CreateSpellCast("Judgement"),
+                            CreateSpellCast("Crusader Strike", ret => Me.CurrentHolyPower < 3)
+                            )),
+
+                    // AoE Routine - I know the EJ guide says at 5 but I put it to 3 mainly for dungeons. 
+                    new Decorator(
+                        ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 8) >= 3,
+                        new PrioritySelector(
+                            CreateSpellBuffOnSelf("Inquisition", ret => Me.CurrentHolyPower > 1),
+                            CreateSpellCast("Divine Storm", ret => Me.CurrentHolyPower < 3),
+                            CreateSpellCast("Hammer of Wrath"),
+                            CreateSpellCast("Exorcism", ret => Me.ActiveAuras.ContainsKey("The Art of War")),
+                            CreateSpellCast("Templar's Verdict", ret => Me.CurrentHolyPower > 2 || Me.ActiveAuras.ContainsKey("Hand of Light")),
+                            CreateSpellCast("Judgement"),
+                            CreateSpellCast("Consecration", ret => Me.ManaPercent > 50),
+                            CreateSpellCast("Holy Wrath", ret => Me.ManaPercent > 50)
+                            )),
+
+                    // Undead Routine
+                    new Decorator(
+                        ret => CurrentTargetIsUndeadOrDemon,
+                        new PrioritySelector(
+                            CreateSpellBuffOnSelf("Inquisition", ret => Me.CurrentHolyPower > 1),
+                            CreateSpellCast("Crusader Strike", ret => Me.CurrentHolyPower < 3 && NearbyUnfriendlyUnits.Count(u => u.Distance < 8) < 2),
+                            CreateSpellCast("Divine Storm", ret => Me.CurrentHolyPower < 3 && NearbyUnfriendlyUnits.Count(u => u.Distance < 8) > 2),
+                            CreateSpellCast("Exorcism", ret => Me.ActiveAuras.ContainsKey("The Art of War")),
+                            CreateSpellCast("Hammer of Wrath"),
+                            CreateSpellCast("Templar's Verdict", ret => Me.CurrentHolyPower > 2 || Me.ActiveAuras.ContainsKey("Hand of Light")),
+                            CreateSpellCast("Judgement"),
+                            CreateSpellCast("Holy Wrath", ret => Me.ManaPercent > 50),
+                            CreateSpellCast("Consecration", ret => Me.ManaPercent > 50)
+                            )),
+
+                    // Single Routine - See AoE notes.
+                    new Decorator(
+                        ret => NearbyUnfriendlyUnits.Count(u => u.Distance < 8) < 3,
+                        new PrioritySelector(
+                            CreateSpellBuffOnSelf("Inquisition", ret => Me.CurrentHolyPower > 1),
+                            CreateSpellCast("Crusader Strike", ret => Me.CurrentHolyPower < 3),
+                            CreateSpellCast("Hammer of Wrath"),
+                            CreateSpellCast("Exorcism", ret => Me.ActiveAuras.ContainsKey("The Art of War")),
+                            CreateSpellCast("Templar's Verdict", ret => Me.CurrentHolyPower > 2 || Me.ActiveAuras.ContainsKey("Hand of Light")),
+                            CreateSpellCast("Judgement"),
+                            CreateSpellCast("Holy Wrath", ret => Me.ManaPercent > 50),
+                            CreateSpellCast("Consecration", ret => Me.ManaPercent > 50)
+                            )),
+
+                    //Bot Control
+                    CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
+
+
+                    // to check it was routine prioritizing correctly or just a general logger due to all the green spell spam that is by default :(
+                    //new Action(delegate
+                    //        {
+                    //            Logger.Write("-- END -- ");
+                    //        }
+                    //        )
                     );
         }
+        #endregion
 
+        #region Pull
         [Class(WoWClass.Paladin)]
         [Spec(TalentSpec.RetributionPaladin)]
         [Behavior(BehaviorType.Pull)]
@@ -61,14 +148,15 @@ namespace Singular
         {
             return
                 new PrioritySelector(
-					CreateEnsureTarget(),
-					CreateAutoAttack(true),
-					CreateMoveToAndFace(ret => Me.CurrentTarget),
                     CreateSpellCast("Judgement"),
-					CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
+
+                    // Make sure we're in range, and facing the damned target. (LOS check as well)
+                    CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
                     );
         }
+        #endregion
 
+        #region Combat Buffs
         [Class(WoWClass.Paladin)]
         [Spec(TalentSpec.RetributionPaladin)]
         [Behavior(BehaviorType.CombatBuffs)]
@@ -77,34 +165,28 @@ namespace Singular
         {
             return
                 new PrioritySelector(
-                    CreateSpellBuffOnSelf("Zealotry", ret => Me.CurrentTarget.MaxHealth > Me.MaxHealth * 10),
-                    CreateSpellBuffOnSelf("Avenging Wrath", ret => !Me.HasAura("Zealotry") && NearbyUnfriendlyUnits.Count > 0),
+                    CreateSpellBuffOnSelf("Zealotry", ret => CurrentTargetIsBoss),
+                    CreateSpellCast("Word of Glory", ret => Me.CurrentHolyPower >= 2 && Me.HealthPercent <= 75),
+                    CreateSpellBuffOnSelf("Avenging Wrath"),
                     CreateSpellBuffOnSelf("Lay on Hands", ret => Me.HealthPercent <= SingularSettings.Instance.Paladin.LayOnHandsHealthRet && !Me.HasAura("Forbearance")),
-					CreateSpellBuffOnSelf("Divine Protection", ret => Me.HealthPercent <= SingularSettings.Instance.Paladin.DivineProtectionHealthRet)
+                    CreateSpellBuffOnSelf("Divine Protection", ret => Me.HealthPercent <= SingularSettings.Instance.Paladin.DivineProtectionHealthRet)
                     );
         }
+        #endregion
 
+        #region Pre-Combat Buffs
         [Class(WoWClass.Paladin)]
         [Spec(TalentSpec.RetributionPaladin)]
         [Behavior(BehaviorType.PreCombatBuffs)]
+        [Behavior(BehaviorType.Rest)]
         [Context(WoWContext.All)]
         public Composite CreateRetributionPaladinPreCombatBuffs()
         {
             return
                 new PrioritySelector(
-                    CreateSpellBuffOnSelf(
-                        "Blessing of Kings",
-                        ret => (!Me.HasAura("Blessing of Might") || Me.Auras["Blessing of Might"].CreatorGuid != Me.Guid) &&
-                               !Me.HasAura("Embrace of the Shale Spider") &&
-                               !Me.HasAura("Mark of the Wild") &&
-                               !Me.HasAura("Blessing of Kings")),
-                    CreateSpellBuffOnSelf(
-                        "Blessing of Might",
-                        ret => (!Me.HasAura("Blessing of Kings") || Me.Auras["Blessing of Kings"].CreatorGuid != Me.Guid) &&
-                               !Me.HasAura("Blessing of Might")),
-                    CreateSpellBuffOnSelf("Seal of Truth", ret => !Me.HasAura("Seal of Truth")),
-                    CreateSpellBuffOnSelf("Seal of Righteousness", ret => !Me.HasAura("Seal of Truth") && !Me.HasAura("Seal of Righteousness"))
+                    CreatePaladinBuffComposite()
                     );
         }
+        #endregion
     }
 }

dfe9bb9e06f21d2deb98044747388a8e1872327c
ApocDev <ApocDev@users.noreply.github.com>
2011-03-12 22:10:20 +0000

    Fixed DeathKnight "Death Grip" issues in combat. Only Blood DKs should still cast it while in instances. Outside of instances, all specs will call it.

--- Singular/ClassSpecific/DeathKnight/Common.cs
+++ Singular/ClassSpecific/DeathKnight/Common.cs
@@ -26,14 +26,15 @@ namespace Singular
     {
         #region Pull
 
+        // All DKs should be throwing death grip when not in intances. It just speeds things up, and makes a mess for PVP :)
         [Class(WoWClass.DeathKnight)]
         [Behavior(BehaviorType.Pull)]
 		[Spec(TalentSpec.BloodDeathKnight)]
 		[Spec(TalentSpec.FrostDeathKnight)]
         [Spec(TalentSpec.UnholyDeathKnight)]
 		[Spec(TalentSpec.Lowbie)]
-        [Context(WoWContext.All)]
-        public Composite CreateDeathKnightPull()
+        [Context(WoWContext.Battlegrounds|WoWContext.Normal)]
+        public Composite CreateDeathKnightPvpNormalPull()
         {
             return
                 new PrioritySelector(
@@ -42,6 +43,42 @@ namespace Singular
                     CreateSpellCast("Howling Blast", false),
                     CreateSpellCast("Icy Touch", false),
 					CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
+                    );
+        }
+
+        // Non-blood DKs shouldn't be using Death Grip in instances. Only tanks should!
+        // You also shouldn't be a blood DK if you're DPSing. Thats just silly. (Like taking a prot war as DPS... you just don't do it)
+        [Class(WoWClass.DeathKnight)]
+        [Behavior(BehaviorType.Pull)]
+        [Spec(TalentSpec.FrostDeathKnight)]
+        [Spec(TalentSpec.UnholyDeathKnight)]
+        [Spec(TalentSpec.Lowbie)]
+        [Context(WoWContext.Instances)]
+        public Composite CreateDeathKnightInstancePull()
+        {
+            return
+                new PrioritySelector(
+                    CreateFaceUnit(),
+                    CreateSpellCast("Howling Blast", false),
+                    CreateSpellCast("Icy Touch", false),
+                    CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
+                    );
+        }
+
+        // Blood DKs should be DG'ing everything it can when pulling. ONLY IN INSTANCES.
+        [Class(WoWClass.DeathKnight)]
+        [Behavior(BehaviorType.Pull)]
+        [Spec(TalentSpec.BloodDeathKnight)]
+        [Context(WoWContext.Instances)]
+        public Composite CreateBloodDeathKnightInstancePull()
+        {
+            return
+                new PrioritySelector(
+                    CreateFaceUnit(),
+                    CreateSpellCast("Death Grip", ret => Me.CurrentTarget.Distance > 15, false),
+                    CreateSpellCast("Howling Blast", false),
+                    CreateSpellCast("Icy Touch", false),
+                    CreateMoveToAndFace(5f, ret => Me.CurrentTarget)
                     );
         }
 
@@ -58,6 +95,8 @@ namespace Singular
 		[Context(WoWContext.All)]
 		public Composite CreateDeathKnightPreCombatBuffs()
 		{
+            // Note: This is one of few places where this is slightly more valid than making multiple functions.
+            // Since this type of stuff is shared, we are safe to do this. Jus leave as-is.
 			return
 				new PrioritySelector(
 					CreateSpellBuffOnSelf(

1004b0302ffe100ffce9cb98b59d9106307f09ed
nuok <nuok@honorbuddy.com>
2011-04-05 13:57:44 +0000

    Basic marksmanship hunter routine

---
+++ Singular/ClassSpecific/Hunter/Marks.cs
@@ -0,0 +1,64 @@
+﻿#region Revision Info
+
+// This file is part of Singular - A community driven Honorbuddy CC
+// $Author: Nuok $
+// $Date: 2011-03-18 16:36:36 +0000 (Fri, 18 Mar 2011) $
+// $HeadURL: http://svn.apocdev.com/singular/trunk/Singular/ClassSpecific/Hunter/Marksman.cs $
+// $LastChangedBy: Nuok $
+
+#endregion
+
+using Styx.Combat.CombatRoutine;
+
+using TreeSharp;
+
+namespace Singular
+{
+    partial class SingularRoutine
+    {
+        [Class(WoWClass.Hunter)]
+        [Spec(TalentSpec.MarksmanshipHunter)]
+        [Behavior(BehaviorType.Combat)]
+        [Context(WoWContext.All)]
+        public Composite CreateMarksmanshipCombat()
+        {
+            WantedPet = "1";
+            return new PrioritySelector(
+                CreateEnsureTarget(),
+                CreateWaitForCast(true),
+				CreateAutoAttack(true),
+                CreateHunterBackPedal(),
+                CreateMoveToAndFace(35f, ret => Me.CurrentTarget),
+                CreateSpellCast("Raptor Strike", ret => Me.CurrentTarget.DistanceSqr < 5 * 5),
+                // Always keep it up on our target!
+                CreateSpellBuff("Hunter's Mark"),
+                // Heal pet when below 70
+                CreateSpellCast("Mend Pet", ret => Me.Pet.HealthPercent < 70 && !Me.Pet.HasAura("Mend Pet")),
+                CreateSpellCast(
+                    "Concussive Shot",
+                    ret => Me.CurrentTarget.CurrentTarget == null || Me.CurrentTarget.CurrentTarget == Me),
+				CreateSpellBuff("Serpent Sting"),
+				CreateSpellCast("Chimera Shot", ret => Me.CurrentTarget.HasAura("Serpent Sting")),
+				CreateSpellCast("Aimed Shot", ret => Me.CurrentTarget.HealthPercent > 80 || Me.HasAura("Ready, Set, Aim...")),
+				CreateSpellCast("Arcane Shot"),
+                CreateSpellCast("Steady Shot")
+                );
+        }
+		[Class(WoWClass.Hunter)]
+        [Spec(TalentSpec.MarksmanshipHunter)]
+		[Behavior(BehaviorType.Pull)]
+        [Context(WoWContext.All)]
+        public Composite CreateMarksmanshipPull()
+		{
+            WantedPet = "1";
+            return new PrioritySelector(
+                CreateEnsureTarget(),
+                CreateWaitForCast(true),
+                CreateMoveToAndFace(35f, ret => Me.CurrentTarget),
+                CreateSpellBuff("Hunter's Mark"),
+				CreateSpellCast("Aimed Shot"),
+				CreateAutoAttack(true)		
+                );
+        }			
+    }
+}

0f34400e78bb2bbc38b48b9bdbe4fcd770e39dd6
bobby53 <bobby53@honorbuddy.com>
2011-04-13 21:15:29 +0000

    refined Elemental rotation to check time remaining on Flame Shock and added support for Fulmination at 7 stacks

--- Singular/ClassSpecific/Shaman/Elemental.cs
+++ Singular/ClassSpecific/Shaman/Elemental.cs
@@ -36,18 +36,17 @@ namespace Singular
         public Composite CreateElementalShamanPull()
         {
             return new PrioritySelector(
-                CreateEnsureTarget(),
-                CreateMoveToAndFace(39, ret => Me.CurrentTarget),
-                CreateWaitForCast(true),
-
+            CreateEnsureTarget(),
+            CreateMoveToAndFace(40, ret => Me.CurrentTarget),
+            CreateWaitForCast(true),
+                //Totems
                 new Decorator(
-                    ret => TotemManager.TotemsInRange == 0,
+                    ret => TotemManager.TotemsInRange == 0 && SpellManager.HasSpell("Call of the Elements"),
                     new Sequence(
                         new Action(ret => TotemManager.SetupTotemBar()),
                         new Action(ret => TotemManager.CallTotems()))),
-
-                CreateSpellCast("Lightning Bolt")
-                );
+            CreateSpellCast("Lightning Bolt")
+            );
         }
 
         [Class(WoWClass.Shaman)]
@@ -76,7 +75,8 @@ namespace Singular
                 // add check for reinforcements ( elem totems )
 
                 // clip the debuff if less than 2 secs remaining (don't chance a LvB with no FS dot)
-                CreateSpellBuff("Flame Shock", ret => !HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(2), 1)),         
+                //  .. note:  flame shock doesn't stack, so be sure to pass 0 for stack test
+                CreateSpellBuff("Flame Shock", ret => !HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(2), 0)),         
 
                 // unleash elements buff is lower dps than additional procs by lightning bolt
                 // .. it is best used in movement fights as an additional instant 
@@ -86,34 +86,14 @@ namespace Singular
                 CreateSpellCast("Lava Burst"),     
 
                 // atleast (6 lightning shield stacks or no Fulmination talent) and FS has > 5 secs left before casting
+                //  .. note:  flame shock doesn't stack, so be sure to pass 0 for stack test
                 CreateSpellCast("Earth Shock", ret => 
-                    (HasAuraStacks("Lightning Shield", 6) || !HaveTalentFulmination) && HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(5), 1)),
+                    (HasAuraStacks("Lightning Shield", 7) || !HaveTalentFulmination) && HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(5), 0)),
 
                 CreateSpellCast("Chain Lightning", ret => WillChainLightningHop()),
 
                 CreateSpellCast("Lightning Bolt")
                 );
-        }
-		
-		[Class(WoWClass.Shaman)]
-        [Spec(TalentSpec.ElementalShaman)]
-        [Spec(TalentSpec.Lowbie)]
-        [Context(WoWContext.All)]
-        [Behavior(BehaviorType.Pull)]
-        public Composite CreateElementalShamanPull()
-		{
-            return new PrioritySelector(
-			CreateEnsureTarget(),
-            CreateMoveToAndFace(40, ret => Me.CurrentTarget),
-            CreateWaitForCast(true),
-			//Totems
-                new Decorator(
-                    ret => TotemManager.TotemsInRange == 0 && SpellManager.HasSpell("Call of the Elements"),
-                    new Sequence(
-                        new Action(ret => TotemManager.SetupTotemBar()),
-                        new Action(ret => TotemManager.CallTotems()))),
-			CreateSpellCast("Lightning Bolt")
-            );
         }
 		
         [Class(WoWClass.Shaman)]

49eb5cf0688c003458fd23851facb1bbe498f675
bobby53 <bobby53@honorbuddy.com>
2011-04-13 20:33:05 +0000

    refinement to rotation to better match EJ

--- Singular/ClassSpecific/Shaman/Elemental.cs
+++ Singular/ClassSpecific/Shaman/Elemental.cs
@@ -11,6 +11,9 @@
 
 #endregion
 
+using System;
+using System.Linq;
+
 using Singular.ClassSpecific.Shaman;
 using Singular.Settings;
 
@@ -19,6 +22,8 @@ using Styx.Logic.Combat;
 
 using TreeSharp;
 
+using Action = TreeSharp.Action;
+
 namespace Singular
 {
     partial class SingularRoutine
@@ -26,6 +31,27 @@ namespace Singular
         [Class(WoWClass.Shaman)]
         [Spec(TalentSpec.ElementalShaman)]
         [Spec(TalentSpec.Lowbie)]
+        [Context(WoWContext.All)]
+        [Behavior(BehaviorType.Pull)]
+        public Composite CreateElementalShamanPull()
+        {
+            return new PrioritySelector(
+                CreateEnsureTarget(),
+                CreateMoveToAndFace(39, ret => Me.CurrentTarget),
+                CreateWaitForCast(true),
+
+                new Decorator(
+                    ret => TotemManager.TotemsInRange == 0,
+                    new Sequence(
+                        new Action(ret => TotemManager.SetupTotemBar()),
+                        new Action(ret => TotemManager.CallTotems()))),
+
+                CreateSpellCast("Lightning Bolt")
+                );
+        }
+
+        [Class(WoWClass.Shaman)]
+        [Spec(TalentSpec.ElementalShaman)]
         [Context(WoWContext.All)]
         [Behavior(BehaviorType.Combat)]
         public Composite CreateElementalShamanCombat()
@@ -47,12 +73,24 @@ namespace Singular
                         new Action(ret => TotemManager.SetupTotemBar()),
                         new Action(ret => TotemManager.CallTotems()))),
 
-                CreateSpellCast("Elemental Mastery"),
-                CreateSpellBuff("Flame Shock"),
-                CreateSpellCast("Unleash Elements"),
-                CreateSpellCast("Lava Burst"),
-                CreateSpellCast("Earth Shock", ret => HasAuraStacks("Lightning Shield", 6) || TalentManager.GetCount(1,8) == 0),
-				CreateSpellBuffOnSelf("Lightning Shield"),
+                // add check for reinforcements ( elem totems )
+
+                // clip the debuff if less than 2 secs remaining (don't chance a LvB with no FS dot)
+                CreateSpellBuff("Flame Shock", ret => !HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(2), 1)),         
+
+                // unleash elements buff is lower dps than additional procs by lightning bolt
+                // .. it is best used in movement fights as an additional instant 
+                // CreateSpellCast("Unleash Elements"),    // if we have this, make sure its before Flame Shock
+
+                CreateSpellCast("Elemental Mastery"),  
+                CreateSpellCast("Lava Burst"),     
+
+                // atleast (6 lightning shield stacks or no Fulmination talent) and FS has > 5 secs left before casting
+                CreateSpellCast("Earth Shock", ret => 
+                    (HasAuraStacks("Lightning Shield", 6) || !HaveTalentFulmination) && HasMyAura("Flame Shock", Me.CurrentTarget, TimeSpan.FromSeconds(5), 1)),
+
+                CreateSpellCast("Chain Lightning", ret => WillChainLightningHop()),
+
                 CreateSpellCast("Lightning Bolt")
                 );
         }

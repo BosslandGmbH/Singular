835ea10ccc81db92c2d09bb47cc1143cbac4d0ca
ApocDev <ApocDev@users.noreply.github.com>
2011-05-02 10:57:50 +0000

    Added 'Spell' class. To handle creating behaviors for spell casting, etc.

---
+++ Singular/Spell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Styx.Logic.Combat;
+using Styx.WoWInternals.WoWObjects;
+
+using TreeSharp;
+
+using Action = TreeSharp.Action;
+
+namespace Singular
+{
+    public delegate WoWUnit UnitSelectionDelegate(object context);
+    public delegate bool SimpleBooleanDelegate(object context);
+
+    class Spell
+    {
+        public static Composite Cast(string name, UnitSelectionDelegate onUnit, SimpleBooleanDelegate requirements)
+        {
+            return new Decorator(
+                ret => requirements(ret) && onUnit(ret) != null && SpellManager.CanCast(name, onUnit(ret), true),
+                new Action(
+                    ret =>
+                        {
+                            Logger.Write("Casting " + name + " on " + onUnit(ret).SafeName());
+                            SpellManager.Cast(name);
+                        })
+                );
+        }
+    }
+}

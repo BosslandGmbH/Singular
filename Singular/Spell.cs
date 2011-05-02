c3592b8303a882c242824773eb21ad3a98b978ee
ApocDev <ApocDev@users.noreply.github.com>
2011-05-02 11:03:06 +0000

    Added simple wrappers for Cast (by name and ID)

--- Singular/Spell.cs
+++ Singular/Spell.cs
@@ -1,22 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Revision Info
 
+// This file is part of Singular - A community driven Honorbuddy CC
+// $Author$
+// $Date$
+// $HeadURL$
+// $LastChangedBy$
+// $LastChangedDate$
+// $LastChangedRevision$
+// $Revision$
+
+#endregion
+
+using Styx;
 using Styx.Logic.Combat;
 using Styx.WoWInternals.WoWObjects;
 
 using TreeSharp;
 
-using Action = TreeSharp.Action;
-
 namespace Singular
 {
     public delegate WoWUnit UnitSelectionDelegate(object context);
+
     public delegate bool SimpleBooleanDelegate(object context);
 
-    class Spell
+    internal class Spell
     {
+        #region Cast - by name
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by name. Returns RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "name">The name.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(string name)
+        {
+            return Cast(name, ret => true);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by name, with special requirements. Returns RunStatus.Success if successful,
+        ///   RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "name">The name.</param>
+        /// <param name = "requirements">The requirements.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(string name, SimpleBooleanDelegate requirements)
+        {
+            return Cast(name, ret => StyxWoW.Me.CurrentTarget, requirements);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by name, on a specific unit. Returns
+        ///   RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "name">The name.</param>
+        /// <param name = "onUnit">The on unit.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(string name, UnitSelectionDelegate onUnit)
+        {
+            return Cast(name, onUnit, ret => true);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by name, with special requirements, on a specific unit. Returns
+        ///   RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "name">The name.</param>
+        /// <param name = "onUnit">The on unit.</param>
+        /// <param name = "requirements">The requirements.</param>
+        /// <returns>.</returns>
         public static Composite Cast(string name, UnitSelectionDelegate onUnit, SimpleBooleanDelegate requirements)
         {
             return new Decorator(
@@ -29,5 +93,79 @@ namespace Singular
                         })
                 );
         }
+
+        #endregion
+
+        #region Cast - by ID
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by ID. Returns RunStatus.Success if successful,
+        ///   RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "spellId">Identifier for the spell.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(int spellId)
+        {
+            return Cast(spellId, ret => true);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by ID, with special requirements. Returns
+        ///   RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "spellId">Identifier for the spell.</param>
+        /// <param name = "requirements">The requirements.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(int spellId, SimpleBooleanDelegate requirements)
+        {
+            return Cast(spellId, ret => StyxWoW.Me.CurrentTarget, requirements);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by ID, on a specific unit. Returns
+        ///   RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "spellId">Identifier for the spell.</param>
+        /// <param name = "onUnit">The on unit.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(int spellId, UnitSelectionDelegate onUnit)
+        {
+            return Cast(spellId, onUnit, ret => true);
+        }
+
+        /// <summary>
+        ///   Creates a behavior to cast a spell by ID, with special requirements, on a specific unit.
+        ///   Returns RunStatus.Success if successful, RunStatus.Failure otherwise.
+        /// </summary>
+        /// <remarks>
+        ///   Created 5/2/2011.
+        /// </remarks>
+        /// <param name = "spellId">Identifier for the spell.</param>
+        /// <param name = "onUnit">The on unit.</param>
+        /// <param name = "requirements">The requirements.</param>
+        /// <returns>.</returns>
+        public static Composite Cast(int spellId, UnitSelectionDelegate onUnit, SimpleBooleanDelegate requirements)
+        {
+            return new Decorator(
+                ret => requirements(ret) && onUnit(ret) != null && SpellManager.CanCast(spellId, onUnit(ret), true),
+                new Action(
+                    ret =>
+                        {
+                            Logger.Write("Casting " + spellId + " on " + onUnit(ret).SafeName());
+                            SpellManager.Cast(spellId);
+                        })
+                );
+        }
+
+        #endregion
     }
 }

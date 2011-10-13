f608a47648f0110905fea1b3064c7d0a412e556e
ApocDev <ApocDev@users.noreply.github.com>
2011-10-13 18:12:54 +0000

    Some changes to cat DPS. Should work a bit better than before.
    Still need to re-check the IsSafelyBehind code to ensure it actually works.

--- Singular/ClassSpecific/Druid/FeralCat.cs
+++ Singular/ClassSpecific/Druid/FeralCat.cs
@@ -1,4 +1,6 @@
-﻿using Singular.Dynamics;
+﻿using System.Linq;
+
+using Singular.Dynamics;
 using Singular.Helpers;
 using Singular.Lists;
 using Singular.Managers;
@@ -30,17 +32,21 @@ namespace Singular.ClassSpecific.Druid
                 Movement.CreateFaceTargetBehavior(),
                 //Spell.Cast("Faerie Fire (Feral)", ret=>!Unit.HasAura(StyxWoW.Me.CurrentTarget, "Faerie Fire", 3)),
                 //new Action(ret=>Logger.WriteDebug("Done with FF Going into boss check")),
+                // 
+                Spell.Cast(
+                    "Faerie Fire (Feral)", ret => !StyxWoW.Me.CurrentTarget.HasSunders() && !StyxWoW.Me.CurrentTarget.HasAura("Faerie Fire", 3)),
+                Singular.Helpers.Common.CreateInterruptSpellCast(ret => StyxWoW.Me.CurrentTarget),
 
-                // IsCasting has been fixed a few releases ago to also check for channeled spells. Skull Bash is also not provided in the custom wrappers for the spell manager
-                // So use it by ID
-                Spell.Cast(80965 /*"Skull Bash (Cat)"*/, ret => StyxWoW.Me.CurrentTarget.IsCasting && StyxWoW.Me.CurrentTarget.CanInterruptCurrentSpellCast),
+                // Throw out swipe if we have more than 2 mobs to DPS.
+                Spell.Cast("Swipe", ret => Unit.NearbyUnfriendlyUnits.Count(u => u.IsWithinMeleeRange) > 2),
 
                 new Decorator(
-                    ret => BossList.BossIds.Contains(StyxWoW.Me.CurrentTarget.Entry),
+                    ret => StyxWoW.Me.CurrentTarget.IsBoss(),
                     new PrioritySelector(
                         Spell.Buff("Mangle (Cat)"),
                         Spell.Cast("Tiger's Fury"),
                         Spell.Cast("Berserk"),
+                        Spell.Cast("Shred", ret => StyxWoW.Me.HasAura("Omen of Clarity")),
                         Spell.Buff("Rip", ret => StyxWoW.Me.ComboPoints == 5),
                         Spell.Buff("Rake"),
                         Spell.BuffSelf("Savage Roar", ret => StyxWoW.Me.ComboPoints == 5),
@@ -49,22 +55,21 @@ namespace Singular.ClassSpecific.Druid
                         // Here's how this works. If the mob is a boss, try and get behind it. If we *can't*
                         // get behind it, we should try to move to it. Its really that simple.
                         Movement.CreateMoveBehindTargetBehavior(3f),
-                        Movement.CreateMoveToTargetBehavior(true, 4f))),
+                        Movement.CreateMoveToMeleeBehavior(true))),
 
-                // TODO: Split all this into multiple combat rotation funcs.
-                // TODO: Add targeting helpers (NearbyEnemyUnits)
-                // TODO: Add simple wrappers for elites, bosses, etc
-
-                // For normal 'around town' grinding, this is all we really need.
-                Spell.Cast("Ferocious Bite", ret => StyxWoW.Me.ComboPoints >= 4),
-                Spell.Cast("Berserk"),
-                Spell.Cast("Tiger's Fury"),
-                // Get rake up just because we can.
-                Spell.Buff("Rake"),
-                Spell.Cast("Mangle (Cat)"),
+                new Decorator(
+                    ret => !StyxWoW.Me.CurrentTarget.IsBoss(),
+                    new PrioritySelector(
+                        // For normal 'around town' grinding, this is all we really need.
+                        Spell.Cast("Ferocious Bite", ret => StyxWoW.Me.ComboPoints >= 4),
+                        Spell.Cast("Berserk"),
+                        Spell.Cast("Tiger's Fury"),
+                        // Get rake up just because we can.
+                        Spell.Buff("Rake"),
+                        Spell.Cast("Mangle (Cat)"))),
 
                 // Since we can't get 'behind' mobs, just do this, kaythanks
-                Movement.CreateMoveToTargetBehavior(true, 4f)
+                Movement.CreateMoveToMeleeBehavior(true)
                 );
         }
     }

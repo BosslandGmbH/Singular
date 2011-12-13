1a820d0f264fa2d6808bab1dd1af4210e295363f
ApocDev <ApocDev@users.noreply.github.com>
2011-12-13 17:23:48 +0000

    Re-adding MM

--- Singular/ClassSpecific/Hunter/Marksman.cs
+++ Singular/ClassSpecific/Hunter/Marksman.cs
@@ -1,11 +1,19 @@
-﻿using Singular.Dynamics;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Singular.Dynamics;
 using Singular.Helpers;
 using Singular.Managers;
 using Singular.Settings;
 using Styx;
 using Styx.Combat.CombatRoutine;
 using Styx.Logic.Combat;
+using Styx.Helpers;
+using Styx.Logic.Pathing;
+using Styx.WoWInternals;
+using Styx.WoWInternals.WoWObjects;
 using TreeSharp;
+using Action = TreeSharp.Action;
 
 namespace Singular.ClassSpecific.Hunter
 {
@@ -30,20 +38,62 @@ namespace Singular.ClassSpecific.Hunter
                 Spell.Cast("Raptor Strike", ret => StyxWoW.Me.CurrentTarget.DistanceSqr < 5 * 5),
                 //Interupt
                 Spell.Cast("Silencing Shot", ret => StyxWoW.Me.CurrentTarget.IsCasting),
-                // Always keep it up on our target!
-                Spell.Buff("Hunter's Mark"),
                 // Heal pet when below 70
                 Spell.Cast("Mend Pet", ret => StyxWoW.Me.GotAlivePet && StyxWoW.Me.Pet.HealthPercent < 70 && !StyxWoW.Me.Pet.HasAura("Mend Pet")),
+
+                // Solo & leveling helpers
                 Spell.Cast(
                     "Concussive Shot",
-                    ret => StyxWoW.Me.CurrentTarget.CurrentTarget == null || StyxWoW.Me.CurrentTarget.CurrentTarget == StyxWoW.Me),
-                // Cast only when close to mob	to try and gain aggro with pet
-                Spell.Cast("Kill Command", ret => StyxWoW.Me.CurrentTarget.DistanceSqr < 5 * 5),
+                    ret => (StyxWoW.Me.CurrentTarget.CurrentTarget == null && !StyxWoW.Me.IsInInstance) || StyxWoW.Me.CurrentTarget.CurrentTarget == StyxWoW.Me),
+                Spell.Cast("Kill Command", ret => StyxWoW.Me.CurrentTarget.DistanceSqr < 5 * 5 && !StyxWoW.Me.IsInInstance),
+
+                new Decorator(ret => Clusters.GetClusterCount(StyxWoW.Me.CurrentTarget, Unit.NearbyUnfriendlyUnits, ClusterType.Radius, 10f) >= 3,
+                    new PrioritySelector(
+                        new Decorator(
+                            ret => SpellManager.CanCast("Explosive Trap") && StyxWoW.Me.PowerPercent >= 20,
+                            new PrioritySelector(
+                                Spell.BuffSelf("Trap Launcher"),
+                                new Sequence(
+                                    new Action(ret => Lua.DoString("RunMacroText(\"/cast Explosive Trap\")")),
+                                    new Action(ret => LegacySpellManager.ClickRemoteLocation(StyxWoW.Me.CurrentTarget.Location))))),
+                        Spell.Cast("Multi-Shot", ret => !SpellManager.CanCast("Explosive Trap")),
+                        Spell.Cast("Aimed Shot", ret => StyxWoW.Me.HasAura("Fire!")),
+                        Spell.Cast("Kill Shot"),
+                        Spell.Cast("Steady Shot", ret => !StyxWoW.Me.IsMoving)
+                    )
+                ),
+
+                // Always keep it up on our target (for single target)!
+                Spell.Buff("Hunter's Mark", ret => StyxWoW.Me.CurrentTarget.IsBoss() && !SpellManager.CanCast("Chimera Shot") && !StyxWoW.Me.CurrentTarget.HasAura("Marked for Death")),
                 Spell.Buff("Serpent Sting"),
-                Spell.Cast("Chimera Shot", ret => StyxWoW.Me.CurrentTarget.HasAura("Serpent Sting")),
-                Spell.Cast("Aimed Shot", ret => StyxWoW.Me.CurrentTarget.HealthPercent > 80 || (StyxWoW.Me.HasAura("Ready, Set, Aim...") && StyxWoW.Me.Auras["Ready, Set, Aim..."].StackCount == 5)),
-                Spell.Cast("Arcane Shot"),
-                Spell.Cast("Steady Shot"),
+                Spell.Cast("Chimera Shot", ret =>
+                    !StyxWoW.Me.IsInInstance ||
+                    (
+                        StyxWoW.Me.CurrentTarget.HasAura("Serpent Sting") &&
+                        (
+                            StyxWoW.Me.CurrentTarget.Auras.Values.First(v => v.Name == "Serpent Sting" && v.CreatorGuid == StyxWoW.Me.Guid).TimeLeft.TotalSeconds < 5
+                            || StyxWoW.Me.CurrentTarget.CurrentHealth < 90
+                        )
+                    ) ||
+                    (
+                        !StyxWoW.Me.CurrentTarget.HasAura("Hunter's Mark")
+                        && !StyxWoW.Me.CurrentTarget.HasAura("Marked for Death")
+                    )
+                ),
+                Spell.Cast("Aimed Shot", ret => StyxWoW.Me.HasAura("Fire!")),
+                Spell.Cast("Steady Shot", ret => !StyxWoW.Me.HasAura("Improved Steady Shot") && !StyxWoW.Me.IsMoving),
+                Spell.Cast("Kill Shot", ret => StyxWoW.Me.CurrentTarget.CurrentHealth < 20),
+                Spell.Cast("Rapid Fire", ret => StyxWoW.Me.IsInInstance && !StyxWoW.Me.HasAura("Rapid Fire")),
+                Spell.Cast("Readiness", ret => StyxWoW.Me.IsInInstance && Spell.GetSpellCooldown("Rapid Fire").TotalSeconds > 60),
+                // Focus Dump
+                new Decorator(ret => StyxWoW.Me.PowerPercent >= 65,
+                    new PrioritySelector(
+                        Spell.Cast("Aimed Shot", ret => StyxWoW.Me.CurrentTarget.CurrentHealth > 90 && !StyxWoW.Me.IsMoving),
+                        Spell.Cast("Arcane Shot")
+                    )
+                ),
+                Spell.Cast("Kill Command", ret => StyxWoW.Me.CurrentTarget.HasAura("Resistance is Futile")),
+                Spell.Cast("Steady Shot", ret => !StyxWoW.Me.IsMoving),
                 Movement.CreateMoveToTargetBehavior(true, 35f)
                 );
         }

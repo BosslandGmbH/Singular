a483dec334da7a79f42f2dfefb6071be0d62ab46
HawkerBot <HawkerBot@users.noreply.github.com>
2011-12-26 14:58:08 +0000

    Apoc's Mark of the Wild fix.

--- Singular/ClassSpecific/Druid/Common.cs
+++ Singular/ClassSpecific/Druid/Common.cs
@@ -14,6 +14,22 @@ namespace Singular.ClassSpecific.Druid
         public static ShapeshiftForm WantedDruidForm { get; set; }
 
         [Class(WoWClass.Druid)]
+        [Behavior(BehaviorType.OutOfCombat)]
+        [Spec(TalentSpec.BalanceDruid)]
+        [Spec(TalentSpec.FeralDruid)]
+        [Spec(TalentSpec.FeralTankDruid)]
+        [Spec(TalentSpec.RestorationDruid)]
+        [Spec(TalentSpec.Lowbie)]
+        [Context(WoWContext.All)]
+
+        public static Composite CreateDruidOutOfCombatBuffs()
+        {
+            return new PrioritySelector(
+                Spell.BuffSelf("Mark of the Wild")
+                );
+        }
+
+        [Class(WoWClass.Druid)]
         [Behavior(BehaviorType.PreCombatBuffs)]
         [Spec(TalentSpec.BalanceDruid)]
         [Spec(TalentSpec.FeralDruid)]
@@ -21,18 +37,21 @@ namespace Singular.ClassSpecific.Druid
         [Spec(TalentSpec.RestorationDruid)]
         [Spec(TalentSpec.Lowbie)]
         [Context(WoWContext.All)]
+
         public static Composite CreateDruidBuffComposite()
         {
+
             return new PrioritySelector(
-                Spell.Cast(
-                    "Mark of the Wild",
-                    ret => StyxWoW.Me,
-                    ret =>
-                    Unit.NearbyFriendlyPlayers.Any(
-                        unit =>
-                        !unit.Dead && !unit.IsGhost && unit.IsInMyPartyOrRaid &&
-                        !unit.HasAnyAura("Mark of the Wild", "Embrace of the Shale Spider", "Blessing of Kings")))
-                );
+               Spell.Cast(
+                   "Mark of the Wild",
+                   ret => StyxWoW.Me,
+                   ret =>
+                   (Unit.NearbyFriendlyPlayers.Any(
+                       unit =>
+                       !unit.Dead && !unit.IsGhost && unit.IsInMyPartyOrRaid &&
+                       !unit.HasAnyAura("Mark of the Wild", "Embrace of the Shale Spider", "Blessing of Kings")))
+                       || !StyxWoW.Me.HasAnyAura("Mark of the Wild", "Embrace of the Shale Spider", "Blessing of Kings"))
+               );
         }
     }
 }

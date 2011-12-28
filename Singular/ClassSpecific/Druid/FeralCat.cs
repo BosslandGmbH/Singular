9d9a2297e2b453ad500f302d13a76a424cd6387c
HawkerBot <HawkerBot@users.noreply.github.com>
2011-12-28 23:07:43 +0000

    Less Rake on non-elite targets.

--- Singular/ClassSpecific/Druid/FeralCat.cs
+++ Singular/ClassSpecific/Druid/FeralCat.cs
@@ -124,7 +124,7 @@ namespace Singular.ClassSpecific.Druid
                     ret => StyxWoW.Me.ComboPoints == 5 &&
                             (StyxWoW.Me.CurrentTarget.GetAuraTimeLeft("Rip", true).Seconds <= 4)),
 
-                Spell.Cast("Rake", ret => StyxWoW.Me.CurrentTarget.GetAuraTimeLeft("Rake", true).Seconds < 2),
+                Spell.Cast("Rake", ret => StyxWoW.Me.CurrentTarget.GetAuraTimeLeft("Rake", true).Seconds < 2 && (StyxWoW.Me.CurrentTarget.Elite || StyxWoW.Me.CurrentTarget.IsPlayer)),
 
                 Spell.BuffSelf("Savage Roar"),
 

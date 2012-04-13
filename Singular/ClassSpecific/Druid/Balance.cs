dc41fda9e33d5225974d988291ffc55441094a54
ApocDev <ApocDev@users.noreply.github.com>
2012-04-13 04:47:38 +0000

    Fixed mushroom faction ID

--- Singular/ClassSpecific/Druid/Balance.cs
+++ Singular/ClassSpecific/Druid/Balance.cs
@@ -45,7 +45,7 @@ namespace Singular.ClassSpecific.Druid
 
         static int MushroomCount
         {
-            get { return ObjectManager.GetObjectsOfType<WoWUnit>().Where(o => o.FactionId == 4 && o.Distance <= 40).Count(o => o.CreatedByUnitGuid == StyxWoW.Me.Guid); }
+            get { return ObjectManager.GetObjectsOfType<WoWUnit>().Where(o => o.FactionId == 2203 && o.Distance <= 40).Count(o => o.CreatedByUnitGuid == StyxWoW.Me.Guid); }
         }
 
         static WoWUnit BestAoeTarget

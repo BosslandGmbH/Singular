52178d2934e98fcd0f95a7f397c6c96a303b1b98
bobby53 <bobby53@honorbuddy.com>
2012-08-21 13:49:41 +0000

    quick fix for Mages being detected as Monks

--- Singular/Managers/TalentManager.cs
+++ Singular/Managers/TalentManager.cs
@@ -77,9 +77,9 @@ namespace Singular.Managers
         FuryWarrior = ((int)WoWClass.Warrior << 8) + 1,
         ProtectionWarrior = ((int)WoWClass.Warrior << 8) + 2,
 
-        BrewmasterMonk = ((int)WoWClass.Mage << 8) + 0,
-        MistweaverMonk = ((int)WoWClass.Mage << 8) + 1,
-        WindwalkerMonk = ((int)WoWClass.Mage << 8) + 2,
+        // BrewmasterMonk = ((int)WoWClass.Monk << 8) + 0,
+        // MistweaverMonk = ((int)WoWClass.Monk << 8) + 1,
+        // WindwalkerMonk = ((int)WoWClass.Monk << 8) + 2,
     }
 
     internal static class TalentManager

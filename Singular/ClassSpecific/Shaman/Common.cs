6b277fc38c34463700705cd5c6fbc270ff03d81f
bobby53 <bobby53@honorbuddy.com>
2012-09-08 12:13:16 +0000

    Shaman/Common.cs - added for spec independent support, etc

---
+++ Singular/ClassSpecific/Shaman/Common.cs
@@ -0,0 +1,32 @@
+﻿#region Revision Info
+
+// This file is part of Singular - A community driven Honorbuddy CC
+// $Author$
+// $Date$
+// $HeadURL$
+// $LastChangedBy$
+// $LastChangedDate$
+// $LastChangedRevision$
+// $Revision$
+
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Singular.Helpers;
+using Singular.Managers;
+using Singular.Settings;
+
+using Styx;
+using Styx.Combat.CombatRoutine;
+using Styx.CommonBot;
+using Styx.WoWInternals;
+using Styx.WoWInternals.WoWObjects;
+
+using Styx.TreeSharp;
+using CommonBehaviors.Actions;
+
+namespace Singular.ClassSpecific.Shaman
+{
+}

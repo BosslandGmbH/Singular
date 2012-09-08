89d5e0a2e05c1502610a482a433de3d8436994e8
bobby53 <bobby53@honorbuddy.com>
2012-09-08 13:43:05 +0000

    added func to create racials behaviors for Shaman

--- Singular/ClassSpecific/Shaman/Common.cs
+++ Singular/ClassSpecific/Shaman/Common.cs
@@ -29,4 +29,22 @@ using CommonBehaviors.Actions;
 
 namespace Singular.ClassSpecific.Shaman
 {
+    public static class Common
+    {
+        public static Composite CreateShamanRacialsCombat()
+        {
+            return new PrioritySelector(
+                Spell.BuffSelf("Blood Fury",
+                    ret => SingularSettings.Instance.UseRacials &&
+                        StyxWoW.Me.Race == WoWRace.Orc &&
+                        !StyxWoW.Me.HasAnyAura("Elemental Mastery", "Bloodlust", "Heroism", "Time Warp", "Ancient Hysteria")),
+                Spell.BuffSelf("Berserking",
+                    ret => SingularSettings.Instance.UseRacials &&
+                        StyxWoW.Me.Race == WoWRace.Troll &&
+                        !StyxWoW.Me.HasAnyAura("Elemental Mastery", "Bloodlust", "Heroism", "Time Warp", "Ancient Hysteria")),
+                Spell.BuffSelf("Lifeblood",
+                    ret => SingularSettings.Instance.UseRacials &&
+                        !StyxWoW.Me.HasAnyAura("Lifeblood", "Blood Fury", "Berserking", "Bloodlust", "Heroism", "Time Warp", "Ancient Hysteria")));
+        }
+    }
 }

86f62a0b56e9d96a00f6747483f30c810a69ae06
bobby53 <bobby53@honorbuddy.com>
2012-09-12 07:00:46 +0000

    removed obsolete Totem Bar settings.  added settings to specify Resto Healing Spell %'s

--- Singular/Settings/ShamanSettings.cs
+++ Singular/Settings/ShamanSettings.cs
@@ -27,24 +27,6 @@ namespace Singular.Settings
         {
         }
 
-        #region Category: Totems
-        [Setting]
-        [DefaultValue(WoWTotem.None)]
-        [Category("Totems")]
-        [Description("The totem to use for this slot. Select 'None' for automatic usage.")]
-        public WoWTotem EarthTotem { get; set; }
-        [Setting]
-        [DefaultValue(WoWTotem.None)]
-        [Category("Totems")]
-        [Description("The totem to use for this slot. Select 'None' for automatic usage.")]
-        public WoWTotem WaterTotem { get; set; }
-        [Setting]
-        [DefaultValue(WoWTotem.None)]
-        [Category("Totems")]
-        [Description("The totem to use for this slot. Select 'None' for automatic usage.")]
-        public WoWTotem AirTotem { get; set; }
-        #endregion
-
         #region Category: Enhancement
         [Setting]
         [DefaultValue(CastOn.All)]
@@ -76,5 +58,87 @@ namespace Singular.Settings
         public bool ElementalHeal { get; set; }
 
         #endregion
+
+        #region Category: Restoration
+
+        [Setting]
+        [DefaultValue(45)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Ascendance")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealAscendance { get; set; }
+
+        [Setting]
+        [DefaultValue(15)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Oh Shoot!")]
+        [Description("Health % to cast Oh Shoot Heal (Ancestral Swiftness + Greater Healing Wave).  Disabled if set to 0, on cooldown, or talent not selected.")]
+        public int HealAncestralSwiftness { get; set; }
+
+        [Setting]
+        [DefaultValue(16)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Healing Surge")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealHealingSurge { get; set; }
+
+        [Setting]
+        [DefaultValue(49)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Unleash Elements")]
+        [Description("Health % to cast this ability at. Set to 0 to disable as direct heal, but may still be cast as a buff.")]
+        public int HealUnleashElements { get; set; }
+
+        [Setting]
+        [DefaultValue(60)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Greater Healing Wave")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealGreaterHealingWave { get; set; }
+
+        [Setting]
+        [DefaultValue(48)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Spirit Link Totem")]
+        [Description("Health % to cast this ability at.  Only valid in a group. Set to 0 to disable.")]
+        public int HealSpiritLinkTotem { get; set; }
+
+        [Setting]
+        [DefaultValue(47)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Healing Tide Totem")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealHealingTideTotem { get; set; }
+
+        [Setting]
+        [DefaultValue(70)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Healing Wave")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealHealingWave { get; set; }
+
+        [Setting]
+        [DefaultValue(85)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Healing Stream Totem")]
+        [Description("Health % to cast this ability at. Set to 0 to disable.")]
+        public int HealHealingStreamTotem { get; set; }
+
+        [Setting]
+        [DefaultValue(92)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Chain Heal")]
+        [Description("Health % to cast this ability at. Must heal minimum 2 people in party, 3 in a raid. Set to 0 to disable.")]
+        public int HealChainHeal { get; set; }
+
+        [Setting]
+        [DefaultValue(91)]
+        [Category("Restoration")]
+        [DisplayName("Heal % Healing Rain")]
+        [Description("Health % to cast this ability at. Must heal minimum of 3 people in party, 4 in a raid. Set to 0 to disable.")]
+        public int HealHealingRain { get; set; }
+
+        #endregion
+
     }
 }

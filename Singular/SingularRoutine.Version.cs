3845bf532596eed286278489426fc59fb68304a8
bobby53 <bobby53@honorbuddy.com>
2013-10-23 19:29:05 +0000

    build files from .3142

--- Singular/SingularRoutine.Version.cs
+++ Singular/SingularRoutine.Version.cs
@@ -33,8 +33,8 @@ using Styx.CommonBot.Routines;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.3123")]
-[assembly: AssemblyFileVersion("3.0.0.3123")]
+[assembly: AssemblyVersion("3.0.0.3142")]
+[assembly: AssemblyFileVersion("3.0.0.3142")]
 
 namespace Singular
 {
@@ -51,9 +51,9 @@ namespace Singular
     /// 
     /// Singular SVN Information
     /// -------------------------
-    /// Revision 3123
-    /// Date     2013/10/15 22:58:31
-    /// Range    3122:3123
+    /// Revision 3142
+    /// Date     2013/10/23 15:28:21
+    /// Range    3141:3142
     /// 
     /// </summary>
     public partial class SingularRoutine : CombatRoutine
@@ -65,7 +65,7 @@ namespace Singular
         // return Assembly.GetExecutingAssembly().GetName().Version;
         public static Version GetSingularVersion()
         {
-            return new Version("3.0.0.3123");
+            return new Version("3.0.0.3142");
         }
     }
 }

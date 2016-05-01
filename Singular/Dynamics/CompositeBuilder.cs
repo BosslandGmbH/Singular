686f59dd6ce0de3db433ce8aa7dcab2724ed6bdb
raphudo <raphudo@users.noreply.github.com>
2016-05-01 15:59:35 +0000

    Coroutine support for rotations

--- Singular/Dynamics/CompositeBuilder.cs
+++ Singular/Dynamics/CompositeBuilder.cs
@@ -11,221 +11,233 @@ using Styx;
 
 using Styx.TreeSharp;
 using System.Drawing;
+using System.Threading.Tasks;
+using CommonBehaviors.Actions;
 using Singular.ClassSpecific;
+using Styx.Common;
 
 namespace Singular.Dynamics
 {
-    public static class CompositeBuilder
-    {
-        /// <summary>
-        /// current behavior type during behavior construction
-        /// </summary>
-        public static BehaviorType CurrentBehaviorType { get; set; }
+	public static class CompositeBuilder
+	{
+		/// <summary>
+		/// current behavior type during behavior construction
+		/// </summary>
+		public static BehaviorType CurrentBehaviorType { get; set; }
 
-        /// <summary>
-        /// current method name during behavior construction
-        /// </summary>
-        public static string CurrentBehaviorName { get; set; }
+		/// <summary>
+		/// current method name during behavior construction
+		/// </summary>
+		public static string CurrentBehaviorName { get; set; }
 
-        /// <summary>
-        /// current method name during behavior construction
-        /// </summary>
-        public static int CurrentBehaviorPriority { get; set; }
+		/// <summary>
+		/// current method name during behavior construction
+		/// </summary>
+		public static int CurrentBehaviorPriority { get; set; }
 
-        public static bool SilentBehaviorCreation { get; set; }
-
-
-        private static List<MethodInfo> _methods = new List<MethodInfo>();
+		public static bool SilentBehaviorCreation { get; set; }
 
 
-        public static void InvokeInitializers(WoWClass wowClass, WoWSpec spec, WoWContext context, bool silent = false)
-        {
-            BehaviorType behavior = BehaviorType.Initialize;
+		private static List<MethodInfo> _methods = new List<MethodInfo>();
 
-            if (context == WoWContext.None)
-            {
-                return;
-            }
 
-            SilentBehaviorCreation = silent;
+		public static void InvokeInitializers(WoWClass wowClass, WoWSpec spec, WoWContext context, bool silent = false)
+		{
+			BehaviorType behavior = BehaviorType.Initialize;
 
-            // only load methods once
-            if (_methods.Count <= 0)
-            {
-                // Logger.WriteDebug("Singular Behaviors: building method list");
-                foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
-                {
-                    // All behavior methods should not be generic, and should have zero parameters, with their return types being of type Composite.
-                    _methods.AddRange(
-                        type.GetMethods(BindingFlags.Static | BindingFlags.Public).Where(
-                            mi => !mi.IsGenericMethod && mi.GetParameters().Length == 0).Where(
-                                mi => mi.ReturnType.IsAssignableFrom(typeof(Composite))));
-                }
-                Logger.WriteFile("Singular Behaviors: Added " + _methods.Count + " behaviors");
-            }
+			if (context == WoWContext.None)
+			{
+				return;
+			}
 
-            // find all initialization methods for this class/spec/context
-            var matchedMethods = new Dictionary<BehaviorAttribute, MethodInfo>();
-            foreach (MethodInfo mi in _methods)
-            {
-                // If the behavior is set as ignore. Don't use it? Duh?
-                if (mi.GetCustomAttributes(typeof(IgnoreBehaviorCountAttribute), false).Any())
-                    continue;
+			SilentBehaviorCreation = silent;
 
-                // If there's no behavior attrib, then move along.
-                foreach (var a in mi.GetCustomAttributes(typeof(BehaviorAttribute), false))
-                {
-                    var attribute = a as BehaviorAttribute;
-                    if (attribute == null)
-                        continue;
+			// only load methods once
+			if (_methods.Count <= 0)
+			{
+				// Logger.WriteDebug("Singular Behaviors: building method list");
+				foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+				{
+					// All behavior methods should not be generic, and should have zero parameters, with their return types being of type Composite.
+					_methods.AddRange(
+						type.GetMethods(BindingFlags.Static | BindingFlags.Public).Where(
+							mi => !mi.IsGenericMethod && mi.GetCustomAttributes(typeof(BehaviorAttribute)).Any()).Where(
+								mi => typeof(Composite).IsAssignableFrom(mi.ReturnType) || mi.ReturnType == typeof(Task<bool>)));
+				}
+				Logger.WriteFile("Singular Behaviors: Added " + _methods.Count + " behaviors");
+			}
 
-                    // Check if our Initialization behavior matches. If not, don't add it!
-                    if (IsMatchingMethod(attribute, wowClass, spec, behavior, context))
-                    {
-                        if (matchedMethods.ContainsKey(attribute))
-                        {
-                            Logger.Write(LogColor.Hilite, "PROGRAM ERROR: duplicate behaviors. Notify Singular Devs!!!");
-                            Logger.WriteDiagnostic("PROGRAM ERROR: duplicate behaviors. Notify Singular Devs!!!");
-                            Logger.WriteDiagnostic("Error for Attribute: class={0} spec={1} type={2} context={3} priority={4} ",
-                                attribute.SpecificClass,
-                                attribute.SpecificSpec,
-                                attribute.Type,
-                                attribute.SpecificContext,
-                                attribute.PriorityLevel
-                                );
-                            Logger.WriteDiagnostic("    exists {0}:{1}",
-                                matchedMethods[attribute].DeclaringType,
-                                matchedMethods[attribute].Name
-                                );
-                            Logger.WriteDiagnostic("    adding {0}:{1}",
-                                mi.DeclaringType,
-                                mi.Name
-                                );
-                        }
+			// find all initialization methods for this class/spec/context
+			var matchedMethods = new Dictionary<BehaviorAttribute, MethodInfo>();
+			foreach (MethodInfo mi in _methods)
+			{
+				// If the behavior is set as ignore. Don't use it? Duh?
+				if (mi.GetCustomAttributes(typeof(IgnoreBehaviorCountAttribute), false).Any())
+					continue;
 
-                        matchedMethods.Add( attribute, mi);
-                    }
-                }
-            }
+				// If there's no behavior attrib, then move along.
+				foreach (var a in mi.GetCustomAttributes(typeof(BehaviorAttribute), false))
+				{
+					var attribute = a as BehaviorAttribute;
+					if (attribute == null)
+						continue;
 
-            // invoke each initialization behavior in priority order
-            foreach (var kvp in matchedMethods.OrderByDescending(mm => mm.Key.PriorityLevel))
-            {
-                CurrentBehaviorType = behavior;
-                CurrentBehaviorPriority = kvp.Key.PriorityLevel;
-                CurrentBehaviorName = kvp.Value.Name;
-                string invokeInfo = string.Format("{0} {1} {2}", kvp.Key.PriorityLevel.ToString().AlignRight(5), behavior.ToString().AlignLeft(15), kvp.Value.Name);
-                if (!silent)
-                    Logger.WriteFile(invokeInfo);
-                kvp.Value.Invoke(null, null);
-                CurrentBehaviorType = 0;
-                CurrentBehaviorPriority = 0;
-                CurrentBehaviorName = string.Empty;
-            }
+					// Check if our Initialization behavior matches. If not, don't add it!
+					if (IsMatchingMethod(attribute, wowClass, spec, behavior, context))
+					{
+						if (matchedMethods.ContainsKey(attribute))
+						{
+							Logger.Write(LogColor.Hilite, "PROGRAM ERROR: duplicate behaviors. Notify Singular Devs!!!");
+							Logger.WriteDiagnostic("PROGRAM ERROR: duplicate behaviors. Notify Singular Devs!!!");
+							Logger.WriteDiagnostic("Error for Attribute: class={0} spec={1} type={2} context={3} priority={4} ",
+								attribute.SpecificClass,
+								attribute.SpecificSpec,
+								attribute.Type,
+								attribute.SpecificContext,
+								attribute.PriorityLevel
+								);
+							Logger.WriteDiagnostic("    exists {0}:{1}",
+								matchedMethods[attribute].DeclaringType,
+								matchedMethods[attribute].Name
+								);
+							Logger.WriteDiagnostic("    adding {0}:{1}",
+								mi.DeclaringType,
+								mi.Name
+								);
+						}
 
-            return;
-        }
+						matchedMethods.Add(attribute, mi);
+					}
+				}
+			}
 
-        public static Composite GetComposite(WoWClass wowClass, WoWSpec spec, BehaviorType behavior, WoWContext context, out int behaviourCount, bool silent = false)
-        {
-            if (context == WoWContext.None)
-            {
-                // None is an invalid context, but rather than stopping bot wait it out with donothing logic
-                Logger.Write( LogColor.Hilite, "No Active Context -{0}{1} for{2} set to DoNothingBehavior temporarily", wowClass.ToString().CamelToSpaced(), behavior.ToString().CamelToSpaced(), spec.ToString().CamelToSpaced());
-                behaviourCount = 1;
-                return NoContextAvailable.CreateDoNothingBehavior();
-            }
+			// invoke each initialization behavior in priority order
+			foreach (var kvp in matchedMethods.OrderByDescending(mm => mm.Key.PriorityLevel))
+			{
+				CurrentBehaviorType = behavior;
+				CurrentBehaviorPriority = kvp.Key.PriorityLevel;
+				CurrentBehaviorName = kvp.Value.Name;
+				string invokeInfo = string.Format("{0} {1} {2}", kvp.Key.PriorityLevel.ToString().AlignRight(5), behavior.ToString().AlignLeft(15), kvp.Value.Name);
+				if (!silent)
+					Logger.WriteFile(invokeInfo);
+				kvp.Value.Invoke(null, null);
+				CurrentBehaviorType = 0;
+				CurrentBehaviorPriority = 0;
+				CurrentBehaviorName = string.Empty;
+			}
 
-            SilentBehaviorCreation = silent;
-            behaviourCount = 0;
-            var matchedMethods = new Dictionary<BehaviorAttribute, Composite>();
+			return;
+		}
 
-            foreach (MethodInfo mi in _methods)
-            {
-                // If the behavior is set as ignore. Don't use it? Duh?
-                if (mi.GetCustomAttributes(typeof(IgnoreBehaviorCountAttribute), false).Any())
-                    continue;
+		public static Composite GetComposite(WoWClass wowClass, WoWSpec spec, BehaviorType behavior, WoWContext context, out int behaviourCount, bool silent = false)
+		{
+			if (context == WoWContext.None)
+			{
+				// None is an invalid context, but rather than stopping bot wait it out with donothing logic
+				Logger.Write(LogColor.Hilite, "No Active Context -{0}{1} for{2} set to DoNothingBehavior temporarily", wowClass.ToString().CamelToSpaced(), behavior.ToString().CamelToSpaced(), spec.ToString().CamelToSpaced());
+				behaviourCount = 1;
+				return NoContextAvailable.CreateDoNothingBehavior();
+			}
 
-                // If there's no behavior attrib, then move along.
-                foreach (var a in mi.GetCustomAttributes(typeof(BehaviorAttribute), false))
-                {
-                    var attribute = a as BehaviorAttribute;
-                    if (attribute == null)
-                        continue;
+			SilentBehaviorCreation = silent;
+			behaviourCount = 0;
+			var matchedMethods = new Dictionary<BehaviorAttribute, Composite>();
 
-                    // Check if our behavior matches with what we want. If not, don't add it!
-                    if (IsMatchingMethod(attribute, wowClass, spec, behavior, context))
-                    {
-                        if (!silent)
-                            Logger.WriteFile("{0} {1} {2}", attribute.PriorityLevel.ToString().AlignRight(4), behavior.ToString().AlignLeft(15), mi.Name);
+			foreach (MethodInfo mi in _methods)
+			{
+				// If the behavior is set as ignore. Don't use it? Duh?
+				if (mi.GetCustomAttributes(typeof(IgnoreBehaviorCountAttribute), false).Any())
+					continue;
 
-                        CurrentBehaviorType = behavior;
-                        CurrentBehaviorPriority = attribute.PriorityLevel;
-                        CurrentBehaviorName = mi.Name;
+				// If there's no behavior attrib, then move along.
+				foreach (var a in mi.GetCustomAttributes(typeof(BehaviorAttribute), false))
+				{
+					var attribute = a as BehaviorAttribute;
+					if (attribute == null)
+						continue;
 
-                        // if it blows up here, you defined a method with the exact same attribute and priority as one already found
+					// Check if our behavior matches with what we want. If not, don't add it!
+					if (IsMatchingMethod(attribute, wowClass, spec, behavior, context))
+					{
+						if (!silent)
+							Logger.WriteFile("{0} {1} {2}", attribute.PriorityLevel.ToString().AlignRight(4), behavior.ToString().AlignLeft(15), mi.Name);
 
-                        // wrap in trace class
-                        Composite comp = mi.Invoke(null, null) as Composite;
-                        string name = behavior.ToString() + "." + mi.Name + "." + attribute.PriorityLevel.ToString();
+						CurrentBehaviorType = behavior;
+						CurrentBehaviorPriority = attribute.PriorityLevel;
+						CurrentBehaviorName = mi.Name;
 
-                        if (SingularSettings.Trace)
-                            comp = new CallTrace( name, comp);
+						// if it blows up here, you defined a method with the exact same attribute and priority as one already found
 
-                        if (matchedMethods.ContainsKey(attribute))
-                        {
-                            Logger.Write(LogColor.Hilite, "Internal Error:  method '{0}' has attribute that already exists", name);
-                            Logger.WriteDiagnostic(" ");
-                            Logger.WriteDiagnostic("Dump Methods");
-                            Logger.WriteDiagnostic("===========================");
-                            foreach (var v in matchedMethods)
-                            {
-                                Logger.WriteDiagnostic("{0} {1} {2} {3}", v.Key.SpecificClass, v.Key.SpecificSpec, v.Key.SpecificContext, v.Key.PriorityLevel);
-                            }
-                            Logger.WriteDiagnostic("===========================");
-                            Logger.WriteDiagnostic("{0} {1} {2} {3}  == add  attempt for {4}", attribute.SpecificClass, attribute.SpecificSpec, attribute.SpecificContext, attribute.PriorityLevel, name);
-                            Logger.WriteDiagnostic(" ");
-                        }
+						Composite comp;
+						if (typeof(Composite).IsAssignableFrom(mi.ReturnType))
+						{
+							comp = mi.Invoke(null, null) as Composite;
+						}
+						else
+						{
+							Beta.Assert(mi.ReturnType == typeof(Task<bool>));
+							comp = new ActionRunCoroutine(o => mi.Invoke(null, null) as Task<bool>);
+						}
 
-                        matchedMethods.Add(attribute, comp);
+						string name = behavior + "." + mi.Name + "." + attribute.PriorityLevel;
 
-                        CurrentBehaviorType = 0;
-                        CurrentBehaviorPriority = 0;
-                        CurrentBehaviorName = string.Empty;
-                    }
-                }
-            }
-            // If we found no methods, rofls!
-            if (matchedMethods.Count <= 0)
-            {
-                return null;
-            }
+						if (SingularSettings.Trace)
+							comp = new CallTrace(name, comp);
 
-            var result = new PrioritySelector();
-            foreach (var kvp in matchedMethods.OrderByDescending(mm => mm.Key.PriorityLevel))
-            {
-                result.AddChild(kvp.Value);
-                behaviourCount++;
-            }
+						if (matchedMethods.ContainsKey(attribute))
+						{
+							Logger.Write(LogColor.Hilite, "Internal Error:  method '{0}' has attribute that already exists", name);
+							Logger.WriteDiagnostic(" ");
+							Logger.WriteDiagnostic("Dump Methods");
+							Logger.WriteDiagnostic("===========================");
+							foreach (var v in matchedMethods)
+							{
+								Logger.WriteDiagnostic("{0} {1} {2} {3}", v.Key.SpecificClass, v.Key.SpecificSpec, v.Key.SpecificContext, v.Key.PriorityLevel);
+							}
+							Logger.WriteDiagnostic("===========================");
+							Logger.WriteDiagnostic("{0} {1} {2} {3}  == add  attempt for {4}", attribute.SpecificClass, attribute.SpecificSpec, attribute.SpecificContext, attribute.PriorityLevel, name);
+							Logger.WriteDiagnostic(" ");
+						}
 
-            return result;
-        }
+						matchedMethods.Add(attribute, comp);
 
-        private static bool IsMatchingMethod(BehaviorAttribute attribute, WoWClass wowClass, WoWSpec spec, BehaviorType behavior, WoWContext context)
-        {
-            if (attribute.SpecificClass != wowClass && attribute.SpecificClass != WoWClass.None)
-                return false;
-            if ((attribute.Type & behavior) == 0)
-                return false;
-            if ((attribute.SpecificContext & context) == 0)
-                return false;
-            if (attribute.SpecificSpec != (WoWSpec)int.MaxValue && attribute.SpecificSpec != spec)
-                return false;
+						CurrentBehaviorType = 0;
+						CurrentBehaviorPriority = 0;
+						CurrentBehaviorName = string.Empty;
+					}
+				}
+			}
+			// If we found no methods, rofls!
+			if (matchedMethods.Count <= 0)
+			{
+				return null;
+			}
 
-            /* Logger.WriteDebug("IsMatchingMethod({0}, {1}, {2}, {3}) - {4}, {5}, {6}, {7}, {8}", wowClass, spec, behavior,
+			var result = new PrioritySelector();
+			foreach (var kvp in matchedMethods.OrderByDescending(mm => mm.Key.PriorityLevel))
+			{
+				result.AddChild(kvp.Value);
+				behaviourCount++;
+			}
+
+			return result;
+		}
+
+		private static bool IsMatchingMethod(BehaviorAttribute attribute, WoWClass wowClass, WoWSpec spec, BehaviorType behavior, WoWContext context)
+		{
+			if (attribute.SpecificClass != wowClass && attribute.SpecificClass != WoWClass.None)
+				return false;
+			if ((attribute.Type & behavior) == 0)
+				return false;
+			if ((attribute.SpecificContext & context) == 0)
+				return false;
+			if (attribute.SpecificSpec != (WoWSpec)int.MaxValue && attribute.SpecificSpec != spec)
+				return false;
+
+			/* Logger.WriteDebug("IsMatchingMethod({0}, {1}, {2}, {3}) - {4}, {5}, {6}, {7}, {8}", wowClass, spec, behavior,
                 context, attribute.SpecificClass, attribute.SpecificSpec, attribute.Type, attribute.SpecificContext,
                 attribute.PriorityLevel);  */
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }

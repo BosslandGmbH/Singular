297354c09ee2893e5ae30d0e69c75bc883d2c2a5
raphudo <raphudo@users.noreply.github.com>
2016-11-03 06:34:26 +0000

    Versioning

--- Singular/SingularRoutine.Version.cs
+++ Singular/SingularRoutine.Version.cs
@@ -33,8 +33,8 @@ using Styx.CommonBot.Routines;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.0.154")]
-[assembly: AssemblyFileVersion("5.0.0.154")]
+[assembly: AssemblyVersion("5.0.0.165")]
+[assembly: AssemblyFileVersion("5.0.0.165")]
 
 namespace Singular
 {
@@ -51,9 +51,9 @@ namespace Singular
     ///
     /// Singular SVN Information
     /// -------------------------
-    /// Revision 154
-    /// Date     2016/11/02 23:50:51
-    /// Range    154
+    /// Revision 165
+    /// Date     2016/11/03 09:30:24
+    /// Range    165
     ///
     /// </summary>
     public partial class SingularRoutine : CombatRoutine
@@ -65,11 +65,11 @@ namespace Singular
         // return Assembly.GetExecutingAssembly().GetName().Version;
         public static Version GetSingularVersion()
         {
-            return new Version("5.0.0.154");
+            return new Version("5.0.0.165");
         }
         public static string GetSingularBuildTime()
         {
-            return "2016/11/02 23:53:04";
+            return "2016/11/03 09:34:05";
         }
     }
 }
